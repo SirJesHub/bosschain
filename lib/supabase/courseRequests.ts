32334ce00217828e0a9035091abf06f595d46045
SirJesHub <siraprop.jes@gmail.com>
2024-02-26 16:50:53 +0700

    Add supabase call to get course, get full course detail and create course + add RLS policy

---
+++ lib/supabase/courseRequests.ts
@@ -0,0 +1,122 @@
+import { supabaseClient } from "./supabaseClient";
+import {
+  GetUserRoleResponse,
+  UserAuth,
+  WriteRequest,
+  CreateCourseRequest,
+  SupabaseResponse,
+  FullCourseDetail,
+} from "@/models/requestModels";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import { Database } from "@/types/supabase";
+
+const getCourse = async ({
+  userId,
+  token,
+}: UserAuth): Promise<
+  SupabaseResponse<Array<Database["public"]["Tables"]["course"]["Row"] | null>>
+> => {
+  if (!token)
+    return {
+      data: null,
+      statusCode: StatusCodes.UNAUTHORIZED,
+      statusMessage: ReasonPhrases.UNAUTHORIZED,
+      error: "Where is your Clerk token?!!",
+    };
+  const supabase = await supabaseClient(token);
+  const { data, error, status, statusText } = await supabase
+    .from("enrollment")
+    .select(`course (course_id, created_at, title, description, instructor_id)`)
+    .eq("user_id", userId);
+  if (error) {
+    console.log("[getCourese ERROR]: ", error);
+    return {
+      data: null,
+      statusCode: status,
+      statusMessage: statusText,
+      error: error.message,
+    };
+  }
+  return {
+    data: data.map((entry) => entry.course),
+    statusCode: status,
+    statusMessage: statusText,
+    error: null,
+  };
+};
+
+const createCourse = async ({
+  userId,
+  token,
+  title,
+  description,
+}: CreateCourseRequest): Promise<
+  SupabaseResponse<Array<Database["public"]["Tables"]["course"]["Row"]>>
+> => {
+  if (!token)
+    return {
+      data: null,
+      statusCode: StatusCodes.UNAUTHORIZED,
+      statusMessage: ReasonPhrases.UNAUTHORIZED,
+      error: "Where is your Clerk token?!!",
+    };
+  const supabase = await supabaseClient(token);
+  const { data, error, status, statusText } = await supabase
+    .from("course")
+    .insert({
+      title,
+      description: description,
+      instructor_id: userId,
+    })
+    .select();
+  if (error) {
+    console.log("[createCourese ERROR]: ", error);
+    return {
+      data: null,
+      statusCode: status,
+      statusMessage: statusText,
+      error: error.message,
+    };
+  }
+  return {
+    data,
+    statusCode: status,
+    statusMessage: statusText,
+    error: null,
+  };
+};
+
+const getFullCourse = async ({
+  userId,
+  token,
+}: UserAuth): Promise<SupabaseResponse<Array<FullCourseDetail | null>>> => {
+  if (!token)
+    return {
+      data: null,
+      statusCode: StatusCodes.UNAUTHORIZED,
+      statusMessage: ReasonPhrases.UNAUTHORIZED,
+      error: "Where is your Clerk token?!!",
+    };
+  const supabase = await supabaseClient(token);
+  const { data, error, status, statusText } = await supabase
+    .from("enrollment")
+    .select(`course (*, module (*, lesson(*)))`)
+    .eq("user_id", userId);
+  if (error) {
+    console.log("[getFullCourese ERROR]: ", error);
+    return {
+      data: null,
+      statusCode: status,
+      statusMessage: statusText,
+      error: error.message,
+    };
+  }
+  return {
+    data: data.map((entry) => entry.course),
+    statusCode: status,
+    statusMessage: statusText,
+    error: null,
+  };
+};
+
+export { createCourse, getCourse, getFullCourse };
